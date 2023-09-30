c8b54e648fab2bfc167d652258956f698df9d48d
Gioh Kim <gurugio@linecorp.com>
2023-09-30 20:01:13 +0900

    use misc device instead of chrdev

--- samples/rust/rust_scull.rs
+++ samples/rust/rust_scull.rs
@@ -10,13 +10,13 @@ use kernel::prelude::*;
 use kernel::{
     file::{self, File},
     io_buffer::{IoBufferReader, IoBufferWriter},
-    sync::{Arc, ArcBorrow, Mutex},
-    {chrdev, PAGE_SIZE},
+    miscdev,
+    sync::{Arc, ArcBorrow, Mutex, UniqueArc},
+    PAGE_SIZE,
 };
 
 module! {
-    type: RustScull
-,
+    type: RustScull,
     name: "rust_scull",
     author: "Rust for Linux Contributors",
     description: "Rust LDD ch03 scull",
@@ -26,21 +26,36 @@ module! {
 const SCULL_NR_DEVS: usize = 3;
 static _SCULL_BLOCK_SIZE: usize = PAGE_SIZE;
 
-struct _ScullBlock {
+struct ScullDevInner {
+    counter: usize,
     offset: usize,
     data: Vec<u8>,
 }
 
-struct ScullDevInner {
-    block_counter: usize,
-}
-
 // internal info between file operations
 struct ScullDev {
+    minor: usize,
     inner: Mutex<ScullDevInner>,
-    // mutex
-    // cdev -> Registration.inner.cdevs[]
-    // list of ScullBlock
+}
+
+impl ScullDev {
+    fn try_new(num: usize) -> Result<Arc<Self>> {
+        let mut dev = Pin::from(UniqueArc::try_new(Self {
+            minor: num,
+            inner: unsafe {
+                Mutex::new(ScullDevInner {
+                    counter: 0,
+                    offset: 0,
+                    data: Vec::new(),
+                })
+            },
+        })?);
+
+        let pinned = unsafe { dev.as_mut().map_unchecked_mut(|s| &mut s.inner) };
+        kernel::mutex_init!(pinned, "ScullDev::inner");
+
+        Ok(dev.into())
+    }
 }
 
 // unit struct for file operations
@@ -49,43 +64,32 @@ struct RustFile;
 #[vtable]
 impl file::Operations for RustFile {
     type Data = Arc<ScullDev>;
-    //type OpenData = Arc<ScullDev>;
+    type OpenData = Arc<ScullDev>;
 
-    // called when open() is called
-    fn open(_shared: &(), _file: &file::File) -> Result<Self::Data> {
+    fn open(shared: &Arc<ScullDev>, _file: &file::File) -> Result<Self::Data> {
         pr_info!("open is invoked\n");
-        Ok(Self::Data::try_new(ScullDev {
-            inner: unsafe { Mutex::new(ScullDevInner { block_counter: 0 }) },
-        })?)
+        pr_info!("minor: {}\n", shared.minor);
+        Ok(shared.clone())
     }
 
     //
     fn read(
-        shared: ArcBorrow<'_, ScullDev>,
+        _shared: ArcBorrow<'_, ScullDev>,
         _: &File,
         _data: &mut impl IoBufferWriter,
         _offset: u64,
     ) -> Result<usize> {
         pr_info!("read is invoked\n");
-        {
-            let inner = shared.inner.lock();
-            pr_info!("block_counter={}", inner.block_counter);
-        }
         Ok(0)
     }
 
     fn write(
-        shared: ArcBorrow<'_, ScullDev>,
+        _shared: ArcBorrow<'_, ScullDev>,
         _: &File,
         _data: &mut impl IoBufferReader,
         _offset: u64,
     ) -> Result<usize> {
         pr_debug!("write is invoked\n");
-        {
-            let mut inner = shared.inner.lock();
-            inner.block_counter += 1;
-            pr_info!("block_counter={}", inner.block_counter);
-        }
         Ok(0)
     }
 
@@ -95,24 +99,24 @@ impl file::Operations for RustFile {
 }
 
 struct RustScull {
-    _dev: Pin<Box<chrdev::Registration<SCULL_NR_DEVS>>>,
+    _dev: Vec<Pin<Box<miscdev::Registration<RustFile>>>>,
 }
 
 impl kernel::Module for RustScull {
-    fn init(name: &'static CStr, module: &'static ThisModule) -> Result<Self> {
-        pr_info!("rust_scull is loaded\n");
-
-        let mut chrdev_reg: Pin<Box<chrdev::Registration<SCULL_NR_DEVS>>> =
-            chrdev::Registration::new_pinned(name, 0, module)?;
+    fn init(name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
+        pr_info!("{} is loaded\n", name.to_str()?);
 
         // Register the same kind of device twice, we're just demonstrating
         // that you can use multiple minors. There are two minors in this case
         // because its type is `chrdev::Registration<2>`
-        let _ = (0..SCULL_NR_DEVS)
-            .map(|_| chrdev_reg.as_mut().register::<RustFile>().unwrap())
-            .collect::<()>();
+        let mut devs = Vec::try_with_capacity(SCULL_NR_DEVS)?;
+        for i in 0..SCULL_NR_DEVS {
+            let dev = ScullDev::try_new(i)?;
+            let reg = miscdev::Registration::new_pinned(fmt!("rust_scull{}", i), dev)?;
+            devs.try_push(reg)?;
+        }
 
-        Ok(RustScull { _dev: chrdev_reg })
+        Ok(RustScull { _dev: devs })
     }
 }
 
